c3eac207d331ab1c9543ce6cfe1fe659036b92e0
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2022-07-28 18:05:00 +0200

    added error handler middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,57 @@
 import User from '../models/User.js';
+import { StatusCodes } from 'http-status-codes';
+import { BadRequestError, UnAuthenticatedError } from '../errors/index.js';
 
 const register = async (req, res, next) => {
-  res.send('register');
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    throw new BadRequestError('please provide all values');
+  }
+
+  const userAlreadyExists = await User.findOne({ email });
+
+  if (userAlreadyExists) {
+    throw new BadRequestError('Email already in use');
+  }
+
+  const user = await User.create({ name, email, password });
+
+  res.status(StatusCodes.CREATED).json({
+    user: {
+      email: user.email,
+      name: user.name,
+      lastName: user.lastName,
+      location: user.location,
+    },
+    location: user.location,
+  });
 };
 
 const login = async (req, res) => {
-  res.send('login');
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError('Please provide all values');
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    throw new UnAuthenticatedError('Invalid Credentials');
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+
+  if (!isPasswordCorrect) {
+    throw new UnAuthenticatedError('Invalid Credentials');
+  }
+
+  user.password = undefined;
+
+  res.status(StatusCodes.OK).json({ user, location: user.location });
+
+  res.send('login user');
 };
 
 const updateUser = async (req, res) => {
