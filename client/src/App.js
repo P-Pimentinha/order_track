3cd3594fe623fbeadb55644b41d2e95cce1176db
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2022-07-30 17:50:17 +0200

    added protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
-import { Landing, Error, Dashboard, Register } from './pages';
+import { Landing, Error, Register, ProtectedRoute } from './pages';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { AddOrder, AllOrders, Profile, SharedLayout } from './pages/dashboard';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<div>dashboard Hello</div>}></Route>
+          <Route
+            path='/'
+            element={
+              <ProtectedRoute>
+                <SharedLayout />
+              </ProtectedRoute>
+            }
+          >
+            {/* <Route index element={<Stats />} /> */}
+            <Route index element={<AllOrders />} />
+            <Route path='profile' element={<Profile />} />
+            <Route path='add-order' element={<AddOrder />} />
+          </Route>
           <Route path='/register' element={<Register />}></Route>
           <Route path='/landing' element={<Landing />}></Route>
           <Route path='*' element={<Error />}></Route>
---
+++ client/src/pages/dashboard/index.js
@@ -0,0 +1,6 @@
+import AllOrders from './AllOrders.js';
+import Profile from './Profile.js';
+import AddOrder from './AddOrder.js';
+import SharedLayout from './SharedLayout.js';
+
+export { AllOrders, Profile, AddOrder, SharedLayout };
